90d6e25b16fe1026ec919e1c3b2e354bc0c36c56
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-05-27 00:19:01 +0200

    feat: sqs (redis) add source

--- simple_dynamic_strings_redis.c
+++ simple_dynamic_strings_redis.c
@@ -1,5 +1,12 @@
 // Simple Dynamic Strings (Redis) : gestion de chaînes de caractères dynamiques en C
 
+/*
+* Sources vérifiées
+* Code source actuel : github.com/redis/redis/blob/unstable/src/sds.h
+* Code source actuel : github.com/redis/redis/blob/unstable/src/sds.c
+* Vrai commit des size classes (2015) : github.com/redis/redis/commit/f15df8ba5db09bdf4be58c53930799d82120cc34
+*/
+
 struct sdshdr {
     unsigned int len; // longueur de la chaîne (sans le \0)
     unsigned int free; //
