799124b456dd2d28b0e37a0583699e191cacae91
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-06-30 03:50:13 +0200

    feat: add more info. about overflow integer binary search

--- binarySearch.java.util.Arrays.java
+++ binarySearch.java.util.Arrays.java
@@ -10,7 +10,6 @@ public static int binarySearch(int[] a, int key) {
     int high = a.length - 1;
 
     while (low <= high) {
-        //int mid = (low + high) / 2; // NOTE: bug de dépassement arithmétique dans la recherche binaire. risque de dépassement arithmétique
         /*
         En Java, "int" est un entier signé de 32 bits, donc la somme de deux entiers peut dépasser la valeur maximale d'un int (2^31 - 1).
         Du coups, si on cherche dans un tableau de 2 millards d'éléments. À un moment donnée, "low" et "high" peuvent valoir quelque chose commme :
@@ -22,10 +21,27 @@ public static int binarySearch(int[] a, int key) {
         ce qui provoque un "ArrayIndexOutOfBoundsException" en l'utilisant comme index ou pire un résultat silencieusement incorrect.
 
         Pourquoi cela à survécu 9ans :
-        ...
+        Le bug a été introduit dans java.utils.Array.binarySearch() en 1993 et n'a été découvert que en 2006 (wtf?) par Joshua Bloch.
+        La raison de cette longévité est simple en 1993, les tableaux de plus de 2 millards d'éléments n'existaient pas en pratique.
+        Le bug était donc théoriquement présent mais pratiquement inatteignable avec les machines de l'époque.
+
+        int mid = (low + high) / 2; // NOTE: bug de dépassement arithmétique dans la recherche binaire. risque de dépassement arithmétique
+
+        ici high - low est compris entre 0 et Integer.MAX_VALUE (puisque high >= low dans la boucle), donc la soustraction ne déborde jamais.
+        On divise ensuite ce delta par 2, puis on ajoute à low le résultat est forcément entre low et high.
 
         */
+        // variant avec décalage de bits.
         int mid = (low + high) >>> 1; // FIX : utilisation de l'opérateur de décalage pour éviter le dépassement
+
+        /*
+
+
+
+
+        */
+
+
         int midVal = a[mid];
 
         if (midVal < key) {
