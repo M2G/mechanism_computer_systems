9d7c9f2deeec6aa57ee60e31521c980bb3983c9b
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-07-14 04:36:01 +0200

    feat: add method match naive and match linear (wip)

--- glob.cpp
+++ glob.cpp
@@ -2,3 +2,8 @@
  * glob.c : Port en C des deux algorithmes décrits par Russ Cox dans
  * "Glob Matching Can Be Simple And Fast Too" (research.swtch.com/glob)
 */
+
+#include <string_view>
+
+bool match_naive(std::string_view pattern, std::string_view name) {}
+bool match_linear(std::string_view pattern, std::string_view name) {}
