a38ef5bec22626bd6bab6f1823c95a23602ae5ec
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-07-17 04:10:58 +0200

    feat: add case '?' and others

--- glob.cpp
+++ glob.cpp
@@ -3,23 +3,33 @@
  * "Glob Matching Can Be Simple And Fast Too" (research.swtch.com/glob)
 */
 
+#include <print>
 #include <string_view>
 
-bool match_naive(std::string_view pattern, std::string_view name) {
-    std::size_t px = 0, nx = 0;
-    while (px < pattern.size() || nx < name.size()) {
-        if (px < pattern.size()) {
-            char c = pattern[px];
-            if (c == '*') {
-                // ...
-            } else if (c == '?') {
-                // ...
-            } else {
-                // ...
+namespace glob {
+    bool match_naive(std::string_view pattern, std::string_view name) {
+
+        std::println("[naive] pattern={} name={}", pattern, name);
+
+        std::size_t px = 0, nx = 0;
+        while (px < pattern.size() || nx < name.size()) {
+            if (px < pattern.size()) {
+                char c = pattern[px];
+                if (c == '*') {
+                    // recursive ?
+                } else if (c == '?') {
+                    if (nx < name.size()) { ++p; ++nx; continue; }
+                    // ...
+                } else {
+                    if (nx < name.size() && name[nx] == c) { ++p; ++nx; continue; }
+                    // ...
+                }
             }
         }
-    }
 
-    return true;
+        return true;
+    }
+    bool match_linear(std::string_view pattern, std::string_view name) {
+        return true;
+    }
 }
-bool match_linear(std::string_view pattern, std::string_view name) {}
