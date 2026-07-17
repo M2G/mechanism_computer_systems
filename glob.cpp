6b3f5e6b437312464899b336350fed904288afca
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-07-17 04:14:04 +0200

    feat: add same logic for match_linear

--- glob.cpp
+++ glob.cpp
@@ -30,6 +30,28 @@ namespace glob {
         return true;
     }
     bool match_linear(std::string_view pattern, std::string_view name) {
+        std::println("linear pattern={} name={}", pattern, name);
+
+        std::size_t px = 0, nx = 0;
+        std::size_t next_px = 0, next_nx = 0;
+        bool have_star = false;
+
+        while (px < pattern.size() || nx < name.size()) {
+            if (px < pattern.size()) {
+                char c = pattern[px];
+                if (c == '*') {
+                    // ...
+                } else if (c == '?') {
+                    if (nx < name.size()) { ++p; ++nx; continue; }
+                    // ...
+                } else {
+                    if (nx < name.size() && name[nx] == c) { ++p; ++nx; continue; }
+                    // ...
+                }
+                // ..
+            }
+        }
+
         return true;
     }
 }
