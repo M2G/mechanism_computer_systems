6f67a5c9b459c8497a5b524d9a35d0514f042522
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-15 03:46:37 +0200

    feat: cpp to c (and refacto.)

--- pthread/pthread_join.c
+++ pthread/pthread_join.c
@@ -1,16 +1,32 @@
-#include <iostream>
-#include <thread>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
 
-void print_message()
+// just a function which prints statements
+void *print_message(void *arg)
 {
-    std::cout << "Hello from threads\n";
+    printf("Hello from threads\n");
+    return NULL;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::thread t1(print_message);
-
-    t1.join();
-
+    // variable to store the thread
+    pthread_t t1;
+    // thread creation
+    if (pthread_create(&t1, NULL, &print_message, NULL) != 0)
+    {
+        perror("pthread_create failed");
+        return 1;
+    }
+    // wait for the child thread to join main thread
+    printf("Main: waiting for thread to finish...\n");
+    if (pthread_join(t1, NULL) != 0)
+    {
+        perror("pthread_join failed");
+        return 1;
+    }
+    printf("Main: thread has finished, exiting.\n");
     return 0;
 }
