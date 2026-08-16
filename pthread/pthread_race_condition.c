098548296f6045e34b0823209a59dca9622962e2
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-08-16 04:39:37 +0200

    feat: cpp to c (and refacto.)

--- pthread/pthread_race_condition.c
+++ pthread/pthread_race_condition.c
@@ -1,34 +1,59 @@
-#include <iostream>
-#include <thread>
-//#include <mutex>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
 
 int sum = 0;
-//std::mutex mtx;
 
-// augmente la valeur de la variable sum
-void increase_sum()
+// increases value of variable sum
+// NOTE: this function is intentionally NOT thread-safe
+// to demonstrate a race condition on the shared variable "sum"
+void *increase_sum(void *arg)
 {
     for (int i = 0; i < 1000000; i++)
     {
-        // FIX
-        // std::lock_guard<std::mutex> lock(mtx);
         sum++;
     }
+    return NULL;
 }
 
 int main()
 {
-    // création des threads
-    std::thread t1(increase_sum);
-    std::thread t2(increase_sum);
+    // variable to store the threads
+    pthread_t t1, t2;
 
-    // attente de la fin des deux threads
-    t1.join();
-    t2.join();
+    // thread creation
+    int rc1 = pthread_create(&t1, NULL, &increase_sum, NULL);
+    if (rc1 != 0)
+    {
+        fprintf(stderr, "pthread_create failed: %s\n", strerror(rc1));
+        return 1;
+    }
 
-    // NOTE: affichage de sum après que les deux threads ont terminé
-    std::cout << "The total should be 2000000\n";
-    std::cout << "The total sum is " << sum << "\n";
+    int rc2 = pthread_create(&t2, NULL, &increase_sum, NULL);
+    if (rc2 != 0)
+    {
+        fprintf(stderr, "pthread_create failed: %s\n", strerror(rc2));
+        return 1;
+    }
+
+    // wait for the child threads to join main thread
+    int rc3 = pthread_join(t1, NULL);
+    if (rc3 != 0)
+    {
+        fprintf(stderr, "pthread_join failed: %s\n", strerror(rc3));
+    }
+
+    int rc4 = pthread_join(t2, NULL);
+    if (rc4 != 0)
+    {
+        fprintf(stderr, "pthread_join failed: %s\n", strerror(rc4));
+    }
+
+    // print sum after both threads join
+    printf("The total should be 2000000\n");
+    printf("The total sum is %d \n", sum);
 
     return 0;
 }
